ce6051edd8c2dbb239784df6569069194fea6ff7
ZoomL-Olymp <nnahalenok@gmail.com>
2025-03-22 00:42:17 +0300

    feat: Admin panel

--- to_do_app/to_do_list/admin.py
+++ to_do_app/to_do_list/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Task, Category
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'created_at', 'due_date', 'completed')  # Поля в списке
+    list_filter = ('category', 'completed')  # Фильтры справа
+    search_fields = ('title',)  # Поиск по названию
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')  # Отображаемые поля
+    search_fields = ('name',)  # Поиск по имени
