c042474ed73e00755336ccff64b264e465c9f894
ZoomL-Olymp <nnahalenok@gmail.com>
2025-03-24 18:45:59 +0300

    fix: absolute path for .env

--- to_do_app/bot/config_reader.py
+++ to_do_app/bot/config_reader.py
@@ -1,12 +1,18 @@
 from pydantic import SecretStr, Field
 from pydantic_settings import BaseSettings
 from typing import Optional
+import os  # Добавь импорт os
+from dotenv import load_dotenv # Импортируем функцию
+from pathlib import Path
+
+env_path = Path(__file__).resolve().parent.parent / '.env'
+load_dotenv(dotenv_path=env_path)  # Явно загружаем .env
 
 class Settings(BaseSettings):
     bot_token: SecretStr
 
     class Config:
-        env_file = '.env'
+        env_file = env_path
         env_file_encoding = 'utf-8'
 
 config = Settings()
