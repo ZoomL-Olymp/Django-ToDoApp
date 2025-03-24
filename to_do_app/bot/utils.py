142caa6034a4bc0b4c05b944cef7c2d5dc70b129
ZoomL-Olymp <nnahalenok@gmail.com>
2025-03-24 18:10:28 +0300

    dev-feat: Token storage placeholders

---
+++ to_do_app/bot/utils.py
@@ -0,0 +1,16 @@
+# Здесь пока только заглушки для хранения токенов
+class UserData:
+    _tokens = {}
+
+    @classmethod
+    async def save_tokens(cls, user_id, access_token, refresh_token):
+        cls._tokens[user_id] = (access_token, refresh_token)
+
+    @classmethod
+    async def get_tokens(cls, user_id):
+        return cls._tokens.get(user_id, (None, None))
+
+    @classmethod
+    async def delete_tokens(cls, user_id):
+        if user_id in cls._tokens:
+            del cls._tokens[user_id]
