8fc3c758cbb81a4a748672b0bc452277d3d6099c
ZoomL-Olymp <nnahalenok@gmail.com>
2025-03-24 17:30:28 +0300

    fix: removed redundant Celery result backend

--- to_do_app/to_do_app/settings.py
+++ to_do_app/to_do_app/settings.py
@@ -106,7 +106,6 @@ DATABASES = {
 
 # Celery settings
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'db+postgresql://user:password@host:port/dbname'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
